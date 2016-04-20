33a7be713729c18a15de362e01f19d97c707a2c4
Vignesh Kamath <vk970@nyu.edu>
2016-04-20 16:34:09 -0400

    # WARNING: head commit changed in the meantime
    test

--- Demand Paging/src/Paging.java
+++ Demand Paging/src/Paging.java
@@ -1,9 +1,19 @@
+import java.util.*;
+import java.io.*;
 
 public class Paging {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int machine_size=Integer.parseInt(args[0]);
+		int page_size=Integer.parseInt(args[1]);
+		int process_size=Integer.parseInt(args[2]);
+		int J=Integer.parseInt(args[3]);
+		int N=Integer.parseInt(args[4]);
+		String replacement_algo=args[5];
+		
+		File random_numbers=new File("random-numbers.txt");
+		Scanner sc=new Scanner(random_numbers);
 	}
 
 }
